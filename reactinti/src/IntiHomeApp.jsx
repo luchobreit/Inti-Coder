09e06e296ca84b32a7182e6b9205016738596273
luchobreit <luciobreit@gmail.com>
2021-08-17 14:15:50 -0300

    segunda subida

---
+++ reactinti/src/IntiHomeApp.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="Inti">
+      <NavBar />
+    </div>
+
+    
+  );
+}
+
+
+export default App;
