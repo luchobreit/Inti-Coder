926d281873f25b53b8d66f38c0accf738080d124
luchobreit <luciobreit@gmail.com>
2021-08-24 13:41:15 -0300

    quinto commit

--- reactinti/src/IntiHomeApp.jsx
+++ reactinti/src/IntiHomeApp.jsx
@@ -3,13 +3,20 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './containers/ItemListContainer';
-// sada
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
+  const onAdd =(count)=>{
+    if (window.confirm (`Quieres agregar ${count} al carrito?`)){
+      alert ("agregado correctamente");
+      console.log(`se agrego ${count}`);
+  }
+}
   return (
     <div className="Inti">
       <NavBar />
       <ItemListContainer/>
+      <ItemCount initial={1} stock={5} onAdd= {onAdd}/>
     </div>
 
     
---
+++ reactinti/src/components/ItemCount/ItemCount.js
@@ -0,0 +1,41 @@
+import Button from "react-bootstrap/esm/Button"
+import {useState} from "react"
+
+
+function ItemCount({initial, stock, onAdd}) {
+    const [count, setCount] = useState(initial)
+    const handlerAdd = ()=> {
+        setCount(count + 1);
+        if(stock == count){
+            setCount(count)
+            alert (`el stock maximo permitido es ${stock}`)
+        }  
+    }
+    const handlerRm=()=>{
+        setCount(count - 1);
+        if (count == 0){
+            setCount(count)
+        }
+    }
+
+ const handlerOnAdd=()=>{
+    onAdd(count)
+    setCount(initial)
+ }
+    return (
+     <div className="botonera" >
+        <div className= "operaciones">
+            <Button variant="outline-primary" size="sm"onClick={handlerAdd}>+</Button>
+            <label>{count}</label>
+            <Button variant="outline-primary" size="sm" onClick={handlerRm}>-</Button>
+        </div>
+        <div className="carrito">
+            <Button variant="outline-success" size="sm" onClick={handlerOnAdd}>Agregar al carrito</Button>
+        </div>
+        
+
+     </div>
+    )
+}
+
+export default ItemCount
